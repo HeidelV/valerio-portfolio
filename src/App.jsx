51170829c15e8a8227e4d0c277afbe031fc800b7
Heidel Berg Valerio <valerioheidelberg20@gmail.com>
2026-08-17 14:37:00 +0800

    Created the home section: - my name - job title - some other elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
       <Navbar />
+      <Home />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+import "./Home.css"; 
+import graphPaper from "../../assets/elements/graph-paper.png";
+
+function Home() {
+  return (
+    <div className = "container">
+      <img className="graph-paper" src={graphPaper} alt="" />
+      <div classname = "welcome-texts">
+        <h1 className = "welcome-heading"> Heidel Berg Valerio </h1>
+        <p className = "job-title"> Front-End Developer </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
